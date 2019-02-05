54a90c04f0a2946d77b1947da63b39cc20940628
saeedo201 <zoalktoom201@gmail.com>
2019-02-05 21:44:55 +0200

    First commit

--- metasploit.sh
+++ metasploit.sh
@@ -244,9 +244,7 @@ if [ $Zoal -eq 77 ]
 then
 rm -rf $HOME/MetaSploit
 git clone https://github.com/saeedo201/MetaSploit
-clear
 echo -e $cayn #---------->Hi New Update<----------#
-sleep 1
 fi
 if [ $Zoal -eq 00 ]
 then
