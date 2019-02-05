f2d30cd6df14f558caaaee4e4f33033c8d9bce19
saeedo201 <zoalktoom201@gmail.com>
2019-02-05 15:39:39 +0200

    First commit

--- metasploit.sh
+++ metasploit.sh
@@ -245,7 +245,9 @@ then
 rm -rf $HOME/MetaSploit
 sleep 2
 clear
-echo -e "git clone https://github.com/saeedo201/MetaSploit"
+git clone https://github.com/saeedo201/MetaSploit
+clear
+echo -e $cayn #---------->Hi New Update<----------#
 fi
 if [ $Zoal -eq 00 ]
 then
--- up.sh
+++ up.sh
@@ -5,5 +5,7 @@ then
 rm -rf $HOME/MetaSploit
 sleep 2
 clear
-echo -e "git clone https://github.com/saeedo201/MetaSploit"
+git clone https://github.com/saeedo201/MetaSploit
+clear
+echo -e $cayn #---------->Hi New Update<----------#
 fi
